b7cef7d1eab7a6d54f05e6d7870adf834fce4368
subhanshu jha <subhanshujha001@gmail.com>
2025-04-29 16:47:31 +0530

    deployment syntax error

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,7 @@
+'use server'
 import ProductDetail from './ProductDetail';
 
-export default function ProductPage({ params }: { params: { id: string } }) {
-  return <ProductDetail id={params.id} />;
+export default async function ProductPage({ params }: { params:Promise<{ id: string }> }) {
+    const id=(await params).id
+  return <ProductDetail id={id} />;
 }
