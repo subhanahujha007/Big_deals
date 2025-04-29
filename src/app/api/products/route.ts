a3833be6dd0251820c17c3e0ec9f61f6fe951959
subhanshu jha <subhanshujha001@gmail.com>
2025-04-29 16:58:25 +0530

    deployment syntax error

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,11 +3,9 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const res = await fetch('https://fakestoreapi.com/products');
-
     if (!res.ok) {
       throw new Error(`Failed to fetch: ${res.status}`);
     }
-
     const data = await res.json();
     return NextResponse.json(data);
   } catch (error) {
